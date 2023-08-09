1894a264f34bdabe1d672f4e6331c33c6362215c
Illowah <illowahplease@gmail.com>
2023-08-09 10:58:05 -0400

    Version 1.0

---
+++ ProjectOneGAgame/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let hobo = document.getElementById('hobo');
+    let fight = document.getElementById('fight');
+    let club = document.getElementById('club');
+
+    setTimeout(() => {
+        hobo.style.transform = 'translateX(0)';
+        hobo.style.opacity = '1';
+
+        setTimeout(() => {
+            fight.style.transform = 'translateX(0)';
+            fight.style.opacity = '1';
+
+            setTimeout(() => {
+                club.style.transform = 'translateX(0)';
+                club.style.opacity = '1';
+            }, 500); 
+
+        }, 500);
+
+    }, 500); 
+});
+
+function clickedWord(word) {
+    alert("Continue to the next page!");
+    window.location.href = ''
+}
